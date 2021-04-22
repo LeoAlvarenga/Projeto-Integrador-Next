fddc6c1846a7ec3cfcd756454017b5525604dacd
Leonardo Alvarenga <leonardo.alvarenga@cubos.io>
2021-04-21 21:04:27 -0300

    integrating detail page with tmdb api

---
+++ pages/detail/[movieId].tsx
@@ -0,0 +1,73 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import React from 'react';
+import tmdbApi from '../../services/tmdbApi';
+import { formatDateBR, minutesToHours } from '../../utils/formatters';
+import s from './style.module.scss';
+
+interface IMovieResponse {
+    revenue: number;
+    overview: string;
+    poster_path: string;
+    title: string;
+    release_date: string;
+    runtime: string;
+    videos: {
+        results: {
+            key: string;
+        }[];
+    };
+}
+
+interface IDetailPage {
+    movie: IMovieResponse;
+}
+
+const Detail = ({ movie }: IDetailPage) => {
+
+    console.log(movie);
+
+    return (
+        <>
+        <Head>
+            <title>{movie.title}</title>
+        </Head>
+            <div className={s.container}>
+                <img className={s['container__img']} src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="Titulo do filme" />
+                <div className={s['container__content']}>
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                    <div className={s.infoWrapper}>
+                        <div className={s['infoWrapper__info']}>
+                            <span>Data de Lançamento</span>
+                            <p>{formatDateBR(movie.release_date)}</p>
+                        </div>
+                        <div className={s['infoWrapper__info']}>
+                            <span>Duração</span>
+                            <p>{minutesToHours(Number(movie.runtime))}</p>
+                        </div>
+                        <div className={s['infoWrapper__info']}>
+                            <span>Receita</span>
+                            <p>{movie.revenue.toLocaleString('pt-BR', { style: 'currency', currency: 'USD' })}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+
+    console.log({ params });
+
+    const movie: IMovieResponse = (await tmdbApi.get(`/movie/${params?.movieId}?api_key=4bdeca07f5aa7e579560ab19507b93e5&language=pt-BR`)).data;
+
+    return {
+        props: {
+            movie,
+        }
+    }
+}
+
+export default Detail;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 import { MovieCard } from '../components/MovieCard';
 import tmdbApi from '../services/tmdbApi';
+import { formatDateBR } from '../utils/formatters';
 import s from './home.module.scss';
 
 interface IHomeProps {
@@ -22,27 +24,33 @@ export interface IMovie {
 const IndexPage = ({ movieList }: IHomeProps) => {
 
   return (
-    <div className={s.main}>
-      <h1 className={s['main__title']}>Movie List</h1>
-      <div className={s['main__list']}>
-        {
-          movieList.results.map(movie => (
-            <MovieCard key={movie.id} date={movie.release_date} imageCover={movie.poster_path} movieId={movie.id.toString()} overview={movie.overview} title={movie.title} />
-          ))
-        }
+    <>
+    <Head>
+      <title>Lista de Filmes</title>
+    </Head>
+      <div className={s.main}>
+        <h1 className={s['main__title']}>Lista de Filmes</h1>
+        <div className={s['main__list']}>
+          {
+            movieList.results.map(movie => (
+              <MovieCard key={movie.id} date={formatDateBR(movie.release_date)} imageCover={movie.poster_path} movieId={movie.id.toString()} overview={movie.overview} title={movie.title} />
+            ))
+          }
+        </div>
       </div>
-    </div>
+    </>
   );
-} 
+}
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const movieList: IMoviesListResponse = await (await tmdbApi.get(`/movie/popular?api_key=4bdeca07f5aa7e579560ab19507b93e5`)).data;
+  const movieList: IMoviesListResponse = (await tmdbApi.get(`/movie/popular?api_key=4bdeca07f5aa7e579560ab19507b93e5`)).data;
 
-    return {
-      props: {
-        movieList,
-      }
+  return {
+    props: {
+      movieList,
     }
+  }
 }
 
 export default IndexPage
+
